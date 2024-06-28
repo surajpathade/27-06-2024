3fea4089506fd6afa0def5731e84ad9e09883399
surajpathade <spathade058@gmail.com>
2024-06-28 11:05:13 +0530

    first commit

---
+++ src/Example1.java
@@ -0,0 +1,18 @@
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a = 2,  b = 3;
+		
+		int condition1 = (a & b);
+		int condition2 = (a | b);
+		
+		System.out.println(condition1);
+		System.out.println(condition2);
+
+	}
+
+}
---
+++ src/com/javabinaryOperator/Exampale1.java
@@ -0,0 +1,24 @@
+package com.javabinaryOperator;
+
+public class Exampale1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 200,  b = 300,  c = 250,  d = 150;
+		
+		boolean condition1 = (a >= d); 
+		boolean condition2 = (b >= c); 
+		boolean condition3 = (c >= a); 
+		boolean condition4 = (b >= d); 
+		boolean condition5 = (c >= a); 
+		boolean condition6 = (b >= a); 
+		
+		
+		System.out.println(Condition);
+		
+				
+
+	}
+
+}
